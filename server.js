9d8cbe0c315b4f72bb28c1a4ffc0baf6aac57169
Leonard Ge <gw980531@gmail.com>
2019-10-13 17:44:21 +0100

    Setup simple server to serve the front end to users.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+
+// Reroute all traffic to the main app.
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
+
+console.log("Server starting...")
+
+
