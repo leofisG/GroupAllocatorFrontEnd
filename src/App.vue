ab4770f76a16ea4196c36e301737874a01455a81
Leonard Ge <gw980531@gmail.com>
2019-10-13 18:29:45 +0100

    Created simple interface for users to upload the csv.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UploadCSV msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UploadCSV from './components/UploadCSV.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    UploadCSV
   }
 }
 </script>
