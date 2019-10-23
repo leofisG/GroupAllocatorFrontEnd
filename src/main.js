97d417d9fc85fdc3ef6b92ab8e6cc865b7122580
Leonard Ge <gw980531@gmail.com>
2019-10-23 14:05:50 +0100

    Added material design icons.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
