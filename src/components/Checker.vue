7e1d3e29611d86078c2451fd0a2bfb3ececbddc1
Robert Jin <rzj17@ic.ac.uk>
2019-11-07 18:51:34 +0000

    Add ability to check allocations and remove students from groups

---
+++ src/components/Checker.vue
@@ -0,0 +1,58 @@
+<template>
+  <span>
+    <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
+    <v-tooltip right v-if="!loading">
+      <template v-slot:activator="{ on }">
+        <span v-on="on">
+          <v-icon v-if="isValid" color="green">mdi-checkbox-marked-circle</v-icon>
+          <v-icon v-if="!isValid" color="red">mdi-alert-circle</v-icon>
+        </span>
+      </template>
+      <v-list-item v-for="status in validities" v-bind:key="status.name" v-bind:status="status">
+        <v-icon class="px-2" v-if="status.status" color="green">mdi-checkbox-marked-circle</v-icon>
+        <v-icon class="px-2" v-if="!status.status" color="red">mdi-alert-circle</v-icon>
+        {{ status.message }}
+      </v-list-item>
+    </v-tooltip>
+  </span>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import { checkAll } from "../utility/checkers";
+
+export default {
+  name: "group-checker",
+  props: {
+    group: Array
+  },
+  watch: {
+    group() {
+      this.checkValidity();
+    }
+  },
+  data() {
+    return {
+      loading: true,
+      isValid: false,
+      validities: []
+    };
+  },
+  computed: {
+    ...mapState(["filters", "openFilters"])
+  },
+  methods: {
+    checkValidity() {
+      this.validities = checkAll(this.group, this.filters);
+      this.isValid = this.validities.every(e => e.status);
+      this.loading = false;
+    }
+  },
+  mounted() {
+    this.checkValidity();
+  }
+};
+</script>
+
+<style>
+</style>
--- src/store/store.js
+++ src/store/store.js
@@ -6,39 +6,40 @@ import * as mutations from './mutations'
 Vue.use(Vuex)
 
 const state = {
-    debugURL: "",
-    productionURL: "https://organiser-app.herokuapp.com/allocateGroups",
-    parsedStudents: null,
-    parsedHeaders: null,
-    filters: {},
-    openFilters: [
-        {
-          type: "SizeFilter",
-          name: "Size"
-        }
-      ],
-      availableFilters: [
-        {
-          type: "TimeZoneFilter",
-          name: "Timezone"
-        },
-        {
-          type: "AgeFilter",
-          name: "Age"
-        },
-        {
-          type: "GenderFilter",
-          name: "Gender"
-        }
-      ],
-    results: null,
-    warnings: []
+  debugURL: "",
+  productionURL: "https://organiser-app.herokuapp.com/allocateGroups",
+  parsedStudents: null,
+  parsedHeaders: null,
+  filters: {},
+  openFilters: [
+    {
+      type: "SizeFilter",
+      name: "Size"
+    }
+  ],
+  availableFilters: [
+    {
+      type: "TimeZoneFilter",
+      name: "Timezone"
+    },
+    {
+      type: "AgeFilter",
+      name: "Age"
+    },
+    {
+      type: "GenderFilter",
+      name: "Gender"
+    }
+  ],
+  results: null,
+  originalResults: null,
+  warnings: []
 };
 
 const store = new Vuex.Store({
-    state,
-    getters,
-    mutations
+  state,
+  getters,
+  mutations
 })
 
 export default store;
