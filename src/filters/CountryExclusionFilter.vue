c8793244c688d50cc415287b518ddfe6171bab35
Leonard Ge <gw980531@gmail.com>
2019-11-28 21:55:34 +0000

    Added the minimum students from quant background filter and maximum students from the same country filter.

---
+++ src/filters/CountryExclusionFilter.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card width="100%">
+    <v-card-title class="justify-center">Country Exclusion</v-card-title>
+    <v-card-text>
+      Specify the maximum number of students that can be from the same country 
+      here.
+    </v-card-text>
+    <v-list-item>
+      <v-text-field
+        type="number"
+        min="0"
+        label="Max Same Country"
+        clearable
+        v-model="maxSameCountry"
+        @change="validateMaxSameCountry"
+      ></v-text-field>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+
+export default {
+  name: "countryfilter",
+  computed: {
+    ...mapState(["filters"]),
+    ...mapGetters(["studentCount"])
+  },
+  watch: {
+    maxSameCountry: function() {
+      this.updateFilters();
+    },
+  },
+  data: function() {
+    return {
+      maxSameCountry: 1,
+    };
+  },
+  mounted: function() {
+    if ("country" in this.filters) {
+      this.maxSameCountry = parseInt(this.filters.country.split(",")[0])
+    }
+    this.updateFilters();
+  },
+  methods: {
+    validateMaxSameCountry() {
+      if (this.maxSameCountry <= 1) {
+        this.maxSameCountry = 1;
+      }
+      if (this.maxSameCountry >= this.studentCount) {
+        this.maxSameCountry = this.studentCount - 1;
+      }
+    },
+    remove() {
+      this.$store.commit("removeFromFilter", ["country"])
+      this.$store.commit("removeFilter", "CountryExclusionFilter");
+    },
+    updateFilters() {
+      const values =
+            {
+              country: `0, ${this.maxSameCountry}`,
+            };
+      this.$store.commit("updateFilters", values);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
